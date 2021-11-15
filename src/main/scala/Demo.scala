23fcce7dd3898c3558a8a06d931ae8dbadac85cd
Eivind Berger-Nilsen <eivind.bergernilsen@gmail.com>
2021-11-15 22:27:32 +0100

    redesigned whole system.

--- src/main/scala/Demo.scala
+++ src/main/scala/Demo.scala
@@ -1,4 +1,5 @@
 
+import java.lang.Character.UnicodeBlock
 import scala.io.StdIn.readLine
 import scala.util.matching.Regex
 
@@ -30,10 +31,52 @@ import scala.util.matching.Regex
 
 object Demo extends App {
 
+//  println('\u0628'.toLower)
+//  println(Portugese.letters.toSeq.sortBy(_.toInt).map(c => c.toHexString -> c))
+
+//  val csvParser: Regex = "(?<text>[\\S\\s]+?),(?<language>\\w+)".r
+//  val path = "/home/eivind/Nedlastinger/dataset.csv"
+//  val dictionary = Dictionary.readFromFile(csvParser, path).get
+//
+////  while (true) dictionary.classifyLanguage(readLine("Ready: "))
+//  while (true) println(dictionary.findOrigins(readLine("Ready: ")).mkString)
+
+//  val j = Japanese.loadWords("』の諸本は系統に分類される。すなわち、ヶ条（条ともみなされる）からなり、「簡素本」と呼ばれる写本群と、このヶ条を修正し且つ新た" +
+//    "な条項を追加した、ヶ条からなる「拡大本」と呼ばれる写本群の系統である。さらに、「拡大本」と他の史")
+//  val c = Chinese.parseWords("』の諸本は系統に分類される。すなわち、ヶ条（条ともみなされる）からなり、「簡素本」と呼ばれる写本群と、このヶ条を修正し且つ新た" +
+//    "な条項を追加した、ヶ条からなる「拡大本」と呼ばれる写本群の系統である。さらに、「拡大本」と他の史")
+//  val s = Spanish.loadWords("los primeros habitantes se establecieron cerca de  siendo un grupo de mixtecas " +
+//    "zapotecas otomíes y olmecas tras la conquista de méxico la población fue encargada a sebastián y luisa narvaes " +
+//    "se estableció como patrono a san andrés a quién se le creó una escultura en el siglo xvi el  de febrero de  " +
+//    "el virrey luis de velasco y ruiz de alarcón otorgó un título en reconocimiento de las tierras " +
+//    "comunales de los grupos originarios")
+
+//  println(j)
+//  println(c)
+//  println(s)
+
+
   val csvParser: Regex = "(?<text>[\\S\\s]+?),(?<language>\\w+)".r
   val path = "/home/eivind/Nedlastinger/dataset.csv"
-  val dictionary = Dictionary.readFromFile(csvParser, path).get
+  Language.loadFromFile(csvParser, path)
 
-  while (true) dictionary.classifyLanguage(readLine("Ready: "))
+  while(true){
+    val input = readLine("Ready: ")
+    Language.classifyLanguage(input)
+  }
+
+//  def yieldWeight(p0: Double, p: Double, n: Double) = {
+//    (p0 + ((p + p0*n) / (n + p0*n))) / 2
+//  }
+
+//  def yieldWeight(p0: BigDecimal, p:Double, n: Int) =
+//    (p0.pow(2)*n + 2*p0*n + p)/(2*n + 2*p0*n)
+////
+//  println(yieldWeight(0, 2, 5))
+//
+//  Iterator.iterate(yieldWeight(0, 2, 5))(next => yieldWeight(next, 2, 5))
+//    .take(150)
+//    .foreach(println)
+
 }
 
--- src/main/scala/Language.scala
+++ src/main/scala/Language.scala
@@ -1,5 +1,16 @@
-import scala.annotation.switch
-import scala.collection.immutable.NumericRange
+
+
+
+
+import java.lang.Character.UnicodeBlock._
+import java.lang.Character.UnicodeScript.{HIRAGANA => Hiragana, KATAKANA => Katakana, _}
+import java.lang.Character.{UnicodeBlock, UnicodeScript}
+import java.math.{MathContext, RoundingMode}
+import scala.annotation.tailrec
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Using.Manager
+import scala.util.matching.Regex
 
 /**
  * All languages are statically known. The machine cannot model new languages, only deduce origin of words
@@ -15,48 +26,176 @@ import scala.collection.immutable.NumericRange
  */
 
 
-sealed class Language(_alphabet: Seq[Char]) {
-  def this(other: Language) = this(other.alphabet.toSeq)
-  val alphabet: Set[Char] = _alphabet.toSet
-  def upperCase: Set[Char] = alphabet.map(_.toUpper)
-  def lowerCase: Set[Char] = alphabet.map(_.toLower)
+/*
+ * Data-set:
+ *
+ * word | language
+ * ---------------------------
+ * name | english
+ * is   | english, norwegian
+ * ---------------------------
+ *
+ * Sample:
+ *
+ * My name är John Doe.
+ *
+ * p_english = 2/5
+ * p_norwegian = 1/5
+ *
+ */
+
+sealed trait Language { lang:Product =>
+
+  private val entries: mutable.HashMap[String,Word] = mutable.HashMap()
+
+  sealed trait Word extends CharSequence {
+
+    def score: Double
+    val text: String
+
+    entries.updateWith(text) {
+      case Some(word: PrimeWord) => Some(word)
+      case Some(_) => Some(this)
+      case None => Some(this)
+    }
+
+    override def length(): Int = text.length
+    override def charAt(index: Int): Char = text.charAt(index)
+    override def subSequence(start: Int, end: Int): CharSequence = text.subSequence(start, end)
+    override def equals(obj: Any): Boolean = obj match {
+      case other: Word => this.text.equalsIgnoreCase(other.text)
+      case other: String => this.text.equalsIgnoreCase(other)
+      case _ => false
+    }
+  }
+  case class PrimeWord(text: String) extends Word{
+    override val score: Double = 1.0
+    override def toString: String = s"${lang.productPrefix}.Word($text)"
+  }
+  case class HeuristicWord(text: String) extends Word{
+    var score: Double = entries.get(text).map(_.score).getOrElse(0.0)
+    def percent: Int = (BigDecimal(score).round(new MathContext(2, RoundingMode.HALF_UP))*100).toInt
+    override def toString: String = s"${lang.productPrefix}.Word($text p=$percent%)"
+  }
+
+  def loadData(text: String): Seq[PrimeWord] = splitWords(text).map(PrimeWord).toSeq
+  def mayContain(chars: Char*): Boolean
+  protected def splitWords(text: String): Array[String] = text
+    .strip()
+    .toLowerCase
+    .filter(char => char.isLetter || char.isWhitespace || char == '\'')
+    .split("[\\s-]+")
+    .filter(word => word.length > 1 && mayContain(word.toSeq:_*))
 }
 
-case object Thai extends Language('\u0e00' to '\u0e7f')
-case object Korean extends Language(Latin)
-case object Indonesian extends Language('\u1b00' to '\u1b7f')
-case object Spanish extends Language(Latin.alphabet.toSeq ++ Set('á', 'é', 'í', 'ó', 'ú', 'ü'))
-case object Estonian extends Language('a' to 'z')
-case object Russian extends Language('a' to 'z')
-case object Arabic extends Language('\u0600' to '\uFEFF')
-case object Latin extends Language('a' to 'z')
-case object Persian extends Language('\u0621' to '\u06CC')
-case object Japanese extends Language(('\u3000' to '\u309f') ++ ('\u30a0' to '\u30ff') ++ ('\uff00' to '\uffef') ++ ('\u4e00' to '\u9faf'))
-case object Chinese extends Language(('\u2e80' to '\u2fd5') ++ ('\u3190' to '\u319f') ++ ('\u3400' to '\u4dbf') ++ ('\u4e00' to '\u9fcc') ++ ('\uf900' to '\ufaad'))
-case object Hindi extends Language('\u0900' to '\u097f')
-case object French extends Language('a' to 'z')
-case object Turkish extends Language(Latin.alphabet.toSeq ++ Seq('Ç', 'Ğ', 'I', 'İ', 'Ö', 'Ş', 'Ü'))
-case object English extends Language(Latin)
-case object Tamil extends Language('\u0B80' to '\u0BFA')
-case object Romanian extends Language('a' to 'z')
-case object Dutch extends Language(Latin.alphabet.toSeq :+ '\u0132')
-case object Portugese extends Language('a' to 'z')
-case object Pushto extends Language('a' to 'z')
-case object Swedish extends Language(('a' to 'z') ++ Set('æ', 'ø', 'å'))
-case object Urdu extends Language('\u0627' to '\u06D2')
+case object Thai extends Language.Spanning(Language.Letters.thai)
+case object Korean extends Language.Scripted(HANGUL, HAN) with Language.WhitespaceIgnored
+case object Indonesian extends Language.Blocked(BASIC_LATIN)
+case object Spanish extends Language.Spanning(Language.Letters.spanish)
+case object Russian extends Language.Scripted(UnicodeScript.CYRILLIC)
+case object Arabic extends Language.Scripted(UnicodeScript.ARABIC)
+case object Latin extends Language.Blocked(BASIC_LATIN)
+case object Estonian extends Language.Spanning(Language.Letters.estonian)
+case object Dutch extends Language.Spanning(Language.Letters.dutch)
+case object Portugese extends Language.Spanning(Language.Letters.portuguese)
+case object Persian extends Language.Scripted(UnicodeScript.ARABIC)
+case object Japanese extends Language.Scripted(Hiragana, Katakana, HAN) with Language.WhitespaceIgnored
+case object Chinese extends Language.Scripted(HAN) with Language.WhitespaceIgnored
+case object Hindi extends Language.Spanning(Language.Letters.hindi)
+case object French extends Language.Spanning(Language.Letters.french)
+case object Turkish extends Language.Spanning(Language.Letters.turkish)
+case object English extends Language.Spanning(Language.Letters.english)
+case object Tamil extends Language.Spanning(Language.Letters.tamil)
+case object Romanian extends Language.Spanning(Language.Letters.romanian)
+case object Pushto extends Language.Spanning(Language.Letters.pushto)
+case object Swedish extends Language.Spanning(Language.Letters.swedish)
+case object Urdu extends Language.Spanning(Language.Letters.urdu)
+
 object Language{
 
-  def intersect(language: Language*): Language = language
-    .map(_.alphabet)
-    .reduceOption(_ intersect _)
-    .map(alphabet => new Language(alphabet.toSeq))
-    .getOrElse(new Language(Nil))
+  def dictionary: Map[String,Seq[Language#Word]] = values.toSeq.flatMap(_.entries).groupMap(_._1)(_._2)
 
-  def union(language: Language*): Language = language
-    .map(_.alphabet)
-    .reduceOption(_ union _)
-    .map(alphabet => new Language(alphabet.toSeq))
-    .getOrElse(new Language(Nil))
+  def loadFromFile(regex: Regex, path: String): Unit = for{
+    data <- Manager(manager => manager(Source.fromFile(path)).mkString)
+    regex <- regex.findAllMatchIn(data)
+    (lang,text) = regex.group("language") -> regex.group("text")
+  } Language.forName(lang).foreach(_.loadData(text))
+
+  def classifyLanguage(sample: String): Unit = values.map{lang =>
+    val (primes, heuristic) = lang
+      .splitWords(sample)
+      .map(text => lang.entries.getOrElse(text, lang.HeuristicWord(text)))
+      .partitionMap {
+        case self@lang.PrimeWord(text) => Left(self)
+        case self@lang.HeuristicWord(text) => Right(self)
+      }
+
+    val score = primes.map(_.score).sum + heuristic.map(_.score).sum
+
+    (lang, score, primes, heuristic)
+  }
+    .filterNot(_._2 == 0.0)
+    .maxByOption(_._2)
+    .foreach{ case (language, score, primes, heuristic) =>
+
+//      def yieldWeight(p0: BigDecimal, n: Int, n_total: Int) =
+//        (p0.pow(2)*n + 2*p0*n + ((n_total.toDouble - n)/n_total))/(2*n + 2*p0*n)
+      val p = (primes.map(_.score).sum + heuristic.map(_.score).sum) / (primes.length + heuristic.length)
+
+      def yieldWeight(p0: BigDecimal, p:Double, n: Int) =
+        (p0.pow(2)*n + 2*p0*n + p)/(2*n + 2*p0*n)
+
+      for(hw <- heuristic) {
+        hw.score = yieldWeight(hw.score, p, heuristic.length).toDouble
+      }
+
+      println(s"$language: ${primes.mkString("\t")} ${heuristic.mkString("\t")}")
+    }
+
+
+  sealed abstract class Spanning(letters: Iterable[Char]*) extends Language { this:Product =>
+    override def mayContain(chars: Char*): Boolean = chars
+      .forall(letters.flatten.contains)
+  }
+  sealed abstract class Scripted(unicodeScripts: UnicodeScript*) extends Language { this:Product =>
+    override def mayContain(chars: Char*): Boolean = chars
+      .map(char => UnicodeScript.of(char.toInt))
+      .forall(unicodeScripts.contains)
+  }
+  sealed abstract class Blocked(unicodeBlocks: UnicodeBlock*) extends Language { this:Product =>
+    override def mayContain(chars: Char*): Boolean = chars
+      .map(char => UnicodeScript.of(char.toInt))
+      .forall(unicodeBlocks.contains)
+  }
+
+  trait WhitespaceIgnored{ this:Language =>
+    override protected def splitWords(text: String): Array[String] = text
+      .filter(char => char.isLetter && mayContain(char))
+      .map(_.toLower)
+      .map(_.toString)
+      .toArray
+  }
+
+  object Letters{
+    lazy val thai: Set[Char] = Set.range('\u0e00', '\u0e4f')
+    lazy val spanish: Set[Char] = Set.range('a', 'z') ++ Set('ñ','á', 'é', 'í', 'ó', 'ú','ü')
+    lazy val estonian: Set[Char] = Set('a','b','d','e','g','h','i','j','k','l','m','n', 'o','p','r','s','t','u','v','õ','ä','ö','ü')
+    lazy val dutch: Set[Char] = Set.range('a', 'z') ++ Set('á','é','í','ó','ú','à','è','ë','ï','ö','ü','ĳ')
+    lazy val portuguese: Set[Char] = Set.range('a', 'z') ++ Set('á','é','í','ó','ú','ç','â','ê','ô','ã', 'õ','à', 'è', 'ì', 'ò', 'ù')
+    lazy val hindi: Set[Char] = Set.range('\u0900','\u097f') ++ Set.range('\uA8E0','\uA8FF') ++ Set.range('\u1CD0','\u1CFF')
+    lazy val french: Set[Char] = Set.range('a', 'z') ++ Set('ç','é','â','ê','î','ô','û','à','è','ì','ò','ù','ë','ï','ü')
+    lazy val turkish: Set[Char] = Set.range('a','z') ++ Set('ç','ğ','i','ö','ş','ü')
+    lazy val english: Set[Char] = Set.range('a', 'z')
+    lazy val tamil: Set[Char] = Set.range('\u0B80', '\u0BFF') ++ Set.range(0x11FC0.toChar, 0x11FFF.toChar)
+    lazy val romanian: Set[Char] = Set.range('a', 'z') ++ Set('ă','â','î','ș','ț')
+    lazy val swedish: Set[Char] = Set.range('a', 'z') ++ Set('å', 'ä', 'ö', 'é')
+    lazy val urdu: Set[Char] = Set.range('\u0627', '\u06D2')
+    lazy val pushto = Set('\u0627', '\u0622', '\u0628', '\u067E', '\u062A', '\u067C', '\u062B', '\u062C',
+      '\u0686', '\u062D', '\u062E', '\u0685', '\u0681', '\u062F', '\u0689', '\u0630', '\u0631', '\u0693',
+      '\u0632', '\u0698', '\u0696', '\u0633', '\u0634', '\u069A', '\u0635', '\u0636', '\u0637', '\u0638',
+      '\u0639', '\u063A', '\u0641', '\u0642', '\u06A9', '\u06AB', '\u0644', '\u0645', '\u0646', '\u06BC',
+      '\u06BA', '\u0648', '\u0647', '\u06C0', '\u064A', '\u06D0', '\u06CC', '\u06D2', '\u06CD', '\u0626')
+  }
 
   def forName(name: String): Option[Language] = name.strip().toLowerCase match {
     case "thai" => Some(Thai)
